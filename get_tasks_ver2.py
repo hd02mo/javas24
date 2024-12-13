0e72ccc6bef59c08e27784c87a8c0c42dac24c65
FR-wata <youshengd80@gmail.com>
2024-12-13 15:24:30 +0900

    12/13

--- get_tasks_ver2.py
+++ get_tasks_ver2.py
@@ -30,7 +30,7 @@ def get_contests():
     # URLを格納するリスト
     results = []
     while(True):
-        driver.get(f'https://atcoder.jp/contests/archive?lang=ja&page={page}&page2=')
+        driver.get(f'https://atcoder.jp/contests/archive?category=&keyword=Atcoder+Beginner+Contest&page={page}&ratedType=0')
         #過去のコンテストページへのURLのテーブル
         try:
             url_table = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div[3]/div[2]/div/table/tbody")
@@ -90,11 +90,34 @@ def get_tasks(contest_infos):
     return task_infos
 
 #問題の分析
-def analyze_task(task_infos,contest_infos):
+def analyze_task(task_infos,contest_infos,user_infos):
     #繰り返しはコンテストの回数起きる
+    #ABC19までは Task=abc019_1 以降はTask=abc019_a
+    difficulties1 = ["a","b","c","d","e","f","g","h","i","j"]
+    difficulties2 = [1,2,3,4,5,6,7,8,9,10]
+    difficulties = difficulties1
     for info in contest_infos:
         cont_title = contest_infos["コンテスト名"]
-        driver.get(info["URL"]+"/submissions") #提出一覧ページへ遷移
+        for d in difficulties:
+            #提出一覧ページへ遷移ただしACかつユーザを分けている
+            user_set = set()
+            driver.get(info["URL"] + f"/submissions?f.Task=abc300_{d}&f.LanguageName=&f.Status=AC&f.User=") 
+            
+            try:
+                table = driver.find_element(By.XPATH,"/html/body/div[3]/div/div[1]/div[3]/div/div[2]/table/tbody/tr[1]/td[1]/time")
+            except NoSuchElementException:
+
+
+            trs = table.find_elements(By.TAG_NAME,"tr")
+            for tr in trs:
+                date = tr.find_element(By.XPATH,"/td[1]/time").text
+                name = tr.find_element(By.XPATH,"/td[3]/a[1]").text
+                #すでに調べたユーザーでなく、かつレーティング0-1999でコンテスト五回以上参加のアクティブなuserである。
+                if(not (name in user_set) and (name in user_infos)):
+                    user_set.add(name)
+
+
+
 
 #ユーザのレーティング変化を補足する
 def profile_process(url):
@@ -103,13 +126,19 @@ def profile_process(url):
     driver.get(url+"/history")
     check_box = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div[2]/div[1]/label/input")
     check_box.click() #Ratedのみにする
-    driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div[2]/div[2]/div/div[2]/div/table/tbody")
-    driver.find_elements(By.TAG_NAME,"tr")
-    dates.append()
+    table = driver.find_element(By.XPATH,"/html/body/div[1]/div/div[1]/div[2]/div[2]/div/div[2]/div/table/tbody")
+    trs = table.find_elements(By.TAG_NAME,"tr")
+    for tr in trs:
+        date = tr.find_element(By.XPATH,"/td[1]/time").text
+        dates.append(date)
+        rating = tr.find_element(By.XPATH,"/td[4]/span").text
+        ratings.append(rating)
+
+    return dates , ratings
 
 def user_info_collect():
     page = 1
-    user_infos = []
+    user_infos = {}
     while(True):
         #コンテストの参加数が5以上のユーザーだけに絞った
         driver.get(f"https://atcoder.jp/ranking?contestType=algo&f.Affiliation=&f.BirthYearLowerBound=0&f.BirthYearUpperBound=9999&f.CompetitionsLowerBound=5&f.CompetitionsUpperBound=9999&f.Country=&f.HighestRatingLowerBound=0&f.HighestRatingUpperBound=9999&f.RatingLowerBound=0&f.RatingUpperBound=9999&f.UserScreenName=&f.WinsLowerBound=0&f.WinsUpperBound=9999&page={page}")
@@ -124,11 +153,10 @@ def user_info_collect():
             pro_link = users[2].find_elements(By.TAG_NAME, "a")[2]
             href = pro_link.get_attribute("href")  # URLを取得
             user_name = pro_link.find_element(By.TAG_NAME, "span").text
+            dates , ratings = profile_process(href)
 
-            profile_process(href) = 
-
-            if href:  # hrefが存在すればリストに追加
-                user_infos.append({"ユーザー名":user_name,"URL":href})
+            if href:  # hrefが存在すれば辞書に追加
+                user_infos[user_name] = {"レーティング変化":(dates,ratings)}
         page += 1
 
 
